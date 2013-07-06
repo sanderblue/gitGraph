88cd00ce56113975341d19b11fc4cc3332ed8e3a
sanderblue <william.a.blue@gmail.com>
2013-07-06 00:10:30 -0500

    get some data to the DOM and clean up

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
-$(function () {
+$(function($){
 
     var GitGraph = function () {};
 
+    var apiURL = 'https://api.github.com';
+
     GitGraph.prototype.getGitHubData = function(a) {
-        console.log(a);
+        console.log("Main js: ", a);
 
-        return $.ajax({
+        var allUsersURL = 'https://api.github.com/users';
 
+        return $.getJSON(allUsersURL, function (data) {
+            console.log("prototype data: ", data);
         });
     };
 
+    var getGitHubData = function () {
+        // var userSearchURL = apiURL + '/legacy/user/search/sanderblue';
+        var userSearchURL = apiURL + '/users';
 
-    return GitGraph;
+        return $.ajax({
+                url: userSearchURL,
+                type: 'GET',
+                contentType: 'json',
+                dataType: 'json'
+            });
+    };
 
+    // var userRepoURL = apiURL + 'repos/'+ user +'/'+ repo +'/stats/commit_activity';
+
+    $.when(getGitHubData()).done(function (data) {
+        console.log("Promise done: ", data);
+
+        var optionsDiv = $('#users');
+
+        $.each(data, function () {
+            var h = $('<option>').val(this.login).text(this.login);
+
+            $(h).appendTo(optionsDiv);
+        });
+    });
 });
